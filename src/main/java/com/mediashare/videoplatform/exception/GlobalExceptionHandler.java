820abc143309f914e299c50ae5f0f195e691bd26
asilarda <GUE22110@spengergasse.at>
2024-01-19 11:39:46 +0100

    Refactor GlobalExceptionHandler and Fix "No Static Resource" Error
    - Implemented dedicated handlers for IllegalArgumentException, VideoNotFoundException, and RuntimeException in GlobalExceptionHandler.
    - Resolved "No Static Resource" error by changing exception handling mechanism.

--- src/main/java/com/mediashare/videoplatform/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mediashare/videoplatform/exception/GlobalExceptionHandler.java
@@ -1,27 +1,27 @@
 package com.mediashare.videoplatform.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGeneralException(Exception ex) {
-        logger.error("General exception: {}", ex.getMessage(), ex);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<Object> handleIllegalArgumentExcpetion(IllegalArgumentException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
     }
-
-    @ExceptionHandler(VideoNotFoundException.class)
-    public ResponseEntity<String> handleVideoNotFoundException(VideoNotFoundException ex) {
-        logger.error("VideoNotFoundException: {}", ex.getMessage(), ex);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    @ExceptionHandler({VideoNotFoundException.class})
+    public ResponseEntity<Object> handleVideoNotFoundException(VideoNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
     }
 }
